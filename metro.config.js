898280dcf4434430895be27a31871b1d172fe6a7
Hegrove <val.piano99@gmail.com>
2025-08-23 18:39:05 +0200

    Corriger le resolver react-native-reanimated pour web

--- metro.config.js
+++ metro.config.js
@@ -1,2 +1,10 @@
 const { getDefaultConfig } = require('@expo/metro-config');
-module.exports = getDefaultConfig(__dirname);
+
+const config = getDefaultConfig(__dirname);
+
+// Configure resolver for react-native-reanimated web compatibility
+config.resolver.alias = {
+  'react-native-reanimated': 'react-native-reanimated/lib/module/index.web.js',
+};
+
+module.exports = config;
