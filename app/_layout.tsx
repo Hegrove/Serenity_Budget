f86947e0918614395356ee331ae4223b0f5d29a1
Hegrove <val.piano99@gmail.com>
2025-08-23 18:42:12 +0200

    Updated package.json

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,4 @@
 import 'react-native-gesture-handler';
-import { enableScreens } from 'react-native-screens';
-
-enableScreens(true);
-
 import { useEffect } from 'react';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
