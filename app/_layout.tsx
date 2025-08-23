bd286405238b8b6a45e17902a9187846933029d8
Hegrove <val.piano99@gmail.com>
2025-08-23 18:37:26 +0200

    Ajouter les fichiers d'authentification et d'onboarding

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,7 +6,6 @@ export default function RootLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
-      <Stack.Screen name="(tabs)" />
       <Stack.Screen name="onboarding" />
       <Stack.Screen name="auth/login" />
       <Stack.Screen name="auth/register" />
