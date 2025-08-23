cc5fb643920bbc0e8eb3c49f7f2998f9cce96695
Hegrove <val.piano99@gmail.com>
2025-08-23 18:40:59 +0200

    Updated metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,2 @@
 const { getDefaultConfig } = require('@expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// Configure resolver for react-native-reanimated web compatibility
-config.resolver.alias = {
-  'react-native-reanimated': 'react-native-reanimated/lib/module/index.web.js',
-};
-
-module.exports = config;
+module.exports = getDefaultConfig(__dirname);
